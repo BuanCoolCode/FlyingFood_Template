4e11b9072e78231953568088806240899c5b0ff3
BuanCoolCode <buan@coolcode.io>
2024-12-02 18:02:02 +0100

    WATERMELON!!!!

--- Assets/Scripts/FoodCannon.cs
+++ Assets/Scripts/FoodCannon.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class FoodCannon : MonoBehaviour
 {
+    [SerializeField] private Transform shootPoint;
+    [SerializeField] private GameObject food;
 
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newFood = Instantiate(food, shootPoint.position, Random.rotation);
+            newFood.GetComponent<Rigidbody>().velocity = shootPoint.forward * 30;
+        }
+
+    }   
 }
